d30748af3243ab1140c6295d08936bb0ddc4a73b
Roma <roman.kozak2002@gmail.com>
2022-02-17 18:17:54 +0300

    The First task

---
+++ script.js
@@ -0,0 +1,19 @@
+const Apple = 15.678
+const Banana = 123.965
+const Strawberries = 90.2345
+var a = Apple
+var b = Banana
+var x = Strawberries
+
+
+
+
+const maxPrice = Math.max(Apple, Banana, Strawberries)
+console.log(maxPrice)
+
+const minPrice = Math.min(a, b, x)
+console.log(minPrice)
+
+const sumPrices = a + b + x
+console.log(sumPrices) 
+
