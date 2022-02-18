016fc3abcda2b0035802c88e976e9cf9a6c852b7
Roma <roman.kozak2002@gmail.com>
2022-02-18 16:51:39 +0300

    fix

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-const Apple = 15.678
-const Banana = 123.965
-const Strawberries = 90.2345
-let a = Apple
-let b = Banana
-let x = Strawberries
+const Apple = 15.678;
+const Banana = 123.965;
+const Strawberries = 90.2345;
+let a = Apple;
+let b = Banana;
+let x = Strawberries;
 
 
 
 
-const maxPrice = Math.max(Apple, Banana, Strawberries)
-console.log(maxPrice)
-const minPrice = Math.min(a, b, x)
-console.log(minPrice)
-const sumPrices = a + b + x
-console.log(sumPrices) 
-const mathFloorPrices = Math.floor(a) + Math.floor(b) + Math.floor(x)
-console.log(mathFloorPrices)
-const priceToHundred = Math.round(sumPrices/100) * 100
-console.log(priceToHundred)
-const idkPrice = mathFloorPrices % 2
-console.log(!idkPrice)
-const rest = 500 - sumPrices
-console.log(rest)
-const average = (sumPrices / 3).toFixed(2)
-console.log(average)
-const discount = Math.round(Math.random() * (90 - 1) + 1)
-console.log(discount)
-const randomDiscount = sumPrices / 100 * (100 - discount)
-console.log(randomDiscount.toFixed(2))
-const profit = sumPrices / 2 - (sumPrices * (discount / 100))
-console.log(profit.toFixed(3))
+const maxPrice = Math.max(Apple, Banana, Strawberries);
+console.log(maxPrice);
+const minPrice = Math.min(a, b, x);
+console.log(minPrice);
+const sumPrices = a + b + x;
+console.log(sumPrices);
+const mathFloorPrices = Math.floor(a) + Math.floor(b) + Math.floor(x);
+console.log(mathFloorPrices);
+const priceToHundred = Math.round(sumPrices/100) * 100;
+console.log(priceToHundred);
+const idkPrice = mathFloorPrices % 2;
+console.log(!idkPrice);
+const rest = 500 - sumPrices;
+console.log(rest);
+const average = (sumPrices / 3).toFixed(2);
+console.log(average);
+const discount = Math.round(Math.random() * (90 - 1) + 1);
+console.log(discount);
+const randomDiscount = sumPrices / 100 * (100 - discount);
+console.log(randomDiscount.toFixed(2));
+const profit = sumPrices / 2 - (sumPrices * (discount / 100));
+console.log(profit.toFixed(3));
 
    document.writeln(
    ` <b>Максимальна ціна:</b> ${maxPrice} </br>
